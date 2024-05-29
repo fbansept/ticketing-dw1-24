20e8031c76b200518f02233c1d9a50a723b1808c
fbansept <bansept.franck@gmail.com>
2024-05-29 16:08:30 +0200

    ajout utilisateur

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import {
   FormBuilder,
@@ -6,16 +7,32 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatSnackBarModule,
+  ],
   templateUrl: './edit-user.component.html',
   styleUrl: './edit-user.component.scss',
 })
 export class EditUserComponent {
+  http: HttpClient = inject(HttpClient);
   formBuilder: FormBuilder = inject(FormBuilder);
+  snackBar: MatSnackBar = inject(MatSnackBar);
+  router: Router = inject(Router);
 
   formulaire: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -24,4 +41,31 @@ export class EditUserComponent {
     lastname: ['', [Validators.required]],
     role: ['Etudiant', [Validators.required]],
   });
+
+  roleList: string[] = ['Etudiant', 'Gestionnaire', 'Administrateur'];
+
+  onAjoutUtilisateur() {
+    if (this.formulaire.valid) {
+      this.http
+        .post(
+          'http://localhost/backend-angular-ticket-dw1-24/ajout-utilisateur.php',
+          this.formulaire.value
+        )
+        .subscribe({
+          next: (resultat) => {
+            this.snackBar.open("L'utilisateur a bien été ajouté", undefined, {
+              duration: 3000,
+            });
+            this.router.navigateByUrl('/gestion-utilisateurs');
+          },
+          error: (erreur) => {
+            if (erreur.status == 409) {
+              alert(erreur.error.message);
+            } else {
+              alert('Erreur inconnue, contactez votre administrateur');
+            }
+          },
+        });
+    }
+  }
 }
